eafffb1f50e53c9203afd793a2c5dffa52c50dc5
lyuehh <lyuehh@gmail.com>
2019-02-17 15:05:24 +0800

    fix backspace list not work bug 3

--- modules/keyboard.js
+++ modules/keyboard.js
@@ -99,14 +99,16 @@ class Keyboard extends Module {
   listen() {
     this.quill.root.addEventListener('keydown', evt => {
       if (evt.defaultPrevented) return;
-      /*
       const bindings = (this.bindings[evt.key] || []).concat(
         this.bindings[evt.which] || [],
       );
-      */
       // 修改中文输入法下按键被错误映射的问题
-      let bindings = this.bindings[evt.which] || [];
+      // let bindings = this.bindings[evt.which] || [];
       // 修改中文输入法删除时的问题
+      if (evt.which === 229) {
+        bindings = []
+      }
+        /*
       if (evt.key === 'Backspace' && evt.which === 229) {
         // console.log(evt.key, evt.which);
         // console.log(bindings);
@@ -122,6 +124,7 @@ class Keyboard extends Module {
       if (evt.key === 'Enter' && evt.which === 229) {
         bindings = [];
       }
+      */
       const matches = bindings.filter(binding => Keyboard.match(evt, binding));
       // console.log('matches', matches);
       if (matches.length === 0) return;
